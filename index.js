57b23d8286886f48ef1f5e51ecce4764436b7ebd
m7real <masayeedreal@gmail.com>
2022-11-26 07:44:29 +0600

    mongodb config added with env variables

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wo3xvdt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 // test api
 app.get("/", (req, res) => {
   res.send("Ex Mobile Server is running");
