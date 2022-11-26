82c02489d81fe1db2b916554f34c52fcf42611ef
m7real <masayeedreal@gmail.com>
2022-11-26 07:50:29 +0600

    verifyJWT middleware added

--- index.js
+++ index.js
@@ -16,6 +16,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wo3xvdt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// middleware to verify JWT Token
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+async function run() {
+  try {
+// prettier-ignore
+    } finally {
+// prettier-ignore
+
+    }
+}
+
+run().catch((err) => console.error(err));
+
 // test api
 app.get("/", (req, res) => {
   res.send("Ex Mobile Server is running");
