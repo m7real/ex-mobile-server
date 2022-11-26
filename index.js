e3fabe0b5c858ec179d21376a992e1118b966f6b
m7real <masayeedreal@gmail.com>
2022-11-26 08:16:36 +0600

    api to jwt added

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ async function run() {
       }
       next();
     };
+
+    // issue JWT
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        email: email,
+      };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: "7d" });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
   } finally {
 // prettier-ignore
 
